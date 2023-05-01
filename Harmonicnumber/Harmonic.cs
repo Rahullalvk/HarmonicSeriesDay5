4ee1c31d330b0c6a95ee906f47002c5569ba04e6
Rahulalvk <rahulal432@gmail.com>
2023-05-01 01:26:19 -0700

    Harmonic Series

---
+++ Harmonicnumber/Harmonic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonicnumber
+{
+    internal class Harmonic
+    {
+        public static void Displayharmonic()
+        {
+            double har = 0.0;
+            Console.WriteLine("Enter a number");
+            int N = Convert.ToInt32(Console.ReadLine());
+             {
+                for (int i = 1; i < N; i++)
+                {
+
+                    Console.Write("1/" + i + "+");
+                   
+                }
+            }
+        }
+    }
+}
